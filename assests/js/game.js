1463ab8356157b7341bc8b03461dc98bed807578
Harry Gustave <harry23gust@gmail.com>
2021-02-12 21:27:02 -0600

    initial set up

---
+++ assests/js/game.js
@@ -0,0 +1,20 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving youself a message");
+//this will do math and log 20
+
+console.log(10+10);
+// what is this?
+console.log("our robot's name is " + playerName);
+
+
+// this ceates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+
+}
+
+//fight();
